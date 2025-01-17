cada94d6bd215de6d9a3cc2878ddde0aa41336b6
Oxblue <ferdirizaldijp@gmail.com>
2025-01-17 11:25:23 +0900

    Initial Commit

---
+++ app/src/main/java/jp/co/sharp/translate/app/MainActivity.java
@@ -0,0 +1,149 @@
+package jp.co.sharp.translate.app;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toolbar;
+
+import jp.co.sharp.translate.app.voiceui.ScenarioDefinitions;
+import jp.co.sharp.translate.app.voiceui.VoiceUIListenerImpl;
+import jp.co.sharp.translate.app.voiceui.VoiceUIManagerUtil;
+
+import java.util.List;
+
+import jp.co.sharp.android.voiceui.VoiceUIManager;
+import jp.co.sharp.android.voiceui.VoiceUIVariable;
+
+/**
+ * 音声UIを利用した最低限の機能だけ実装したActivity.
+ */
+public class MainActivity extends Activity implements VoiceUIListenerImpl.ScenarioCallback {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    /**
+     * 音声UI制御.
+     */
+    private VoiceUIManager mVUIManager = null;
+    /**
+     * 音声UIイベントリスナー.
+     */
+    private VoiceUIListenerImpl mVUIListener = null;
+    /**
+     * ホームボタンイベント検知.
+     */
+    private HomeEventReceiver mHomeEventReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.v(TAG, "onCreate()");
+        setContentView(R.layout.activity_main);
+
+        //タイトルバー設定.
+        setupTitleBar();
+
+        //ホームボタンの検知登録.
+        mHomeEventReceiver = new HomeEventReceiver();
+        IntentFilter filterHome = new IntentFilter(Intent.ACTION_CLOSE_SYSTEM_DIALOGS);
+        registerReceiver(mHomeEventReceiver, filterHome);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.v(TAG, "onResume()");
+
+        //VoiceUIManagerインスタンス生成.
+        if(mVUIManager == null){
+            mVUIManager = VoiceUIManager.getService(this);
+        }
+        //VoiceUIListenerインスタンス生成.
+        if (mVUIListener == null) {
+            mVUIListener = new VoiceUIListenerImpl(this);
+        }
+        //VoiceUIListenerの登録.
+        VoiceUIManagerUtil.registerVoiceUIListener(mVUIManager, mVUIListener);
+
+        //Scene有効化.
+        VoiceUIManagerUtil.enableScene(mVUIManager, ScenarioDefinitions.SCENE_COMMON);
+
+        //アプリ起動時に発話を実行.
+        VoiceUIManagerUtil.startSpeech(mVUIManager, ScenarioDefinitions.ACC_HELLO);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.v(TAG, "onPause()");
+
+        //バックに回ったら発話を中止する.
+        VoiceUIManagerUtil.stopSpeech();
+
+        //Scene無効化.
+        VoiceUIManagerUtil.disableScene(mVUIManager, ScenarioDefinitions.SCENE_COMMON);
+
+        //VoiceUIListenerの解除.
+        VoiceUIManagerUtil.unregisterVoiceUIListener(mVUIManager, mVUIListener);
+
+        //単一Activityの場合はonPauseでアプリを終了する.
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy()");
+
+        //ホームボタンの検知破棄.
+        this.unregisterReceiver(mHomeEventReceiver);
+
+        //インスタンスのごみ掃除.
+        mVUIManager = null;
+        mVUIListener = null;
+    }
+
+    /**
+     * VoiceUIListenerクラスからのコールバックを実装する.
+     */
+    @Override
+    public void onScenarioEvent(int event, List<VoiceUIVariable> variables) {
+        Log.v(TAG, "onScenarioEvent() : " + event);
+        switch (event) {
+            //必要なイベント毎に実装.
+            case VoiceUIListenerImpl.ACTION_END:
+            case VoiceUIListenerImpl.RESOLVE_VARIABLE:
+            case VoiceUIListenerImpl.ACTION_START:
+            case VoiceUIListenerImpl.ACTION_CANCELLED:
+            case VoiceUIListenerImpl.ACTION_REJECTED:
+            default:
+                break;
+        }
+    }
+
+    /**
+     * ホームボタンの押下イベントを受け取るためのBroadcastレシーバークラス.<br>
+     * <p/>
+     * アプリは必ずホームボタンで終了する.
+     */
+    private class HomeEventReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.v(TAG, "Receive Home button pressed");
+            // ホームボタン押下でアプリ終了する.
+            finish();
+        }
+    }
+
+    /**
+     * タイトルバーを設定する.
+     */
+    private void setupTitleBar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setActionBar(toolbar);
+    }
+
+}
