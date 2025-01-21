7a67efebd63e86ac1adfe54e8d5304902a2f2d8c
tut201818 <sakata.naruya.sd@tut.jp>
2025-01-21 10:46:11 +0900

    logを増やしたりなど

--- app/src/main/java/jp/co/sharp/translate/app/MainActivity.java
+++ app/src/main/java/jp/co/sharp/translate/app/MainActivity.java
@@ -197,19 +197,40 @@ public class MainActivity extends Activity implements VoiceUIListenerImpl.Scenar
      * 翻訳をしてspeakシナリオを開始させる関数
      */
     private void startSpeakScenario(final String original_word){
-        if(speak_flag == 1) return;//すでにspeakシナリオが実行中の場合はリターン
-        if(Objects.equals(original_word,null) || original_word.length() > max_length) return;//original_wordが不正な場合はリターン
+        if(speak_flag == 1){
+            Log.v(TAG, "Speak Scenario Is During Execution");
+            return;//すでにspeakシナリオが実行中の場合はリターン
+        }
+        if(Objects.equals(original_word,null) || original_word.length() > max_length){
+            Log.v(TAG, "Original_word Is Wrong");
+            return;//original_wordが不正な場合はリターン
+        }
 
         final String translated_word = translate(original_word);//original_wordを英訳したen_wordを作成する
-        if(Objects.equals(translated_word, "Error during translation") || Objects.equals(translated_word, null) || translated_word.length() > max_length) return;//translated_wordが不正な場合はリターン
+        if(Objects.equals(translated_word, "Error during translation") || Objects.equals(translated_word, null) || translated_word.length() > max_length){
+            Log.v(TAG, "Translated_word Is Wrong");
+            return;//translated_wordが不正な場合はリターン
+        }
 
         //出力バーにtranslated_wordの内容を表示する
         inputTextValue.setText(translated_word);
 
-        VoiceUIManagerUtil.setMemory(mVUIManager, ScenarioDefinitions.MEM_P_ORIGINAL_WORD, original_word);
-        VoiceUIManagerUtil.setMemory(mVUIManager, ScenarioDefinitions.MEM_P_TRANSLATED_WORD, translated_word);//翻訳前と済みの単語をspeakシナリオの手が届くpメモリに送る
-        int result = VoiceUIManagerUtil.startSpeech(mVUIManager, ScenarioDefinitions.ACC_SPEAK);//speakシナリオを起動する
-        if(result != VoiceUIManager.VOICEUI_ERROR){
+        int result = VoiceUIManagerUtil.setMemory(mVUIManager, ScenarioDefinitions.MEM_P_ORIGINAL_WORD, original_word);//翻訳前の単語をspeakシナリオの手が届くpメモリに送る
+        if(Objects.equals(result,VoiceUIManager.VOICEUI_ERROR)){
+            Log.v(TAG, "Set Original_word Failed");
+            return;//original_wordのpメモリへの保存が失敗したらリターン
+        }
+        result = VoiceUIManagerUtil.setMemory(mVUIManager, ScenarioDefinitions.MEM_P_TRANSLATED_WORD, translated_word);//翻訳後の単語をspeakシナリオの手が届くpメモリに送る
+        if(Objects.equals(result,VoiceUIManager.VOICEUI_ERROR)){
+            Log.v(TAG, "Set Translated_word Failed");
+            return;//translated_wordのpメモリへの保存が失敗したらリターン
+        }
+
+        result = VoiceUIManagerUtil.startSpeech(mVUIManager, ScenarioDefinitions.ACC_SPEAK);//speakシナリオを起動する
+        if(Objects.equals(result,VoiceUIManager.VOICEUI_ERROR)){
+            Log.v(TAG, "Speak Scenario Failed To Start");
+            return;
+        }else{
             speak_flag = 1;//speakシナリオが正常に開始したら立てる
             Log.v(TAG, "Speak Scenario Started");
         }
