bd65563b17cb336483c42e680654db112e6f08cc
Ferdi Rizaldi <ferdirizaldijp@gmail.com>
2025-02-03 11:06:25 +0900

    Update ShowDrawingActivity.java

--- app/src/main/java/jp/co/sharp/workTogether/app/ShowDrawingActivity.java
+++ app/src/main/java/jp/co/sharp/workTogether/app/ShowDrawingActivity.java
@@ -82,9 +82,10 @@ public class ShowDrawingActivity extends Activity implements VoiceUIListenerImpl
         finishButton.setOnClickListener(view -> {
             // Finish the current activity
 
+            // Projector also needs to end button process
+            stopProjector();
             endShowDrawing();
         });
-
     }
 
     @Override
@@ -342,5 +343,21 @@ public class ShowDrawingActivity extends Activity implements VoiceUIListenerImpl
 
         // Return null if no extras are found
         return null;
+    }
+
+    /**
+     * Stops the projector using ProjectorManagerServiceUtil.
+     */
+    private void stopProjector() {
+        Log.v(TAG, "Stopping Projector");
+
+        Intent intent = new Intent();
+        ComponentName componentName = new ComponentName(
+                ProjectorManagerServiceUtil.PACKAGE_NAME,
+                ProjectorManagerServiceUtil.CLASS_NAME);
+        intent.setComponent(componentName);
+        intent.setAction(ProjectorManagerServiceUtil.ACTION_PROJECTOR_END);
+
+        startService(intent);
     }
 }
