d81836c3e24fbadf89fa98709f607311b23b3be1
tut201818 <sakata.naruya.sd@tut.jp>
2025-02-06 13:30:44 +0900

    セッションアクティビティの時間設定を戻した
    残り時間の計算に関する部分が間違っていたのも修正した

--- app/src/main/java/jp/co/sharp/workTogether/app/SessionActivity.java
+++ app/src/main/java/jp/co/sharp/workTogether/app/SessionActivity.java
@@ -58,14 +58,14 @@ public class SessionActivity extends Activity implements VoiceUIListenerImpl.Sce
     private Handler handler;//毎秒呼び出されるスレッドの制御に使用
     private Runnable runnable;//毎秒呼び出されるスレッドの制御に使用
     private boolean timerStopFrag;//毎秒呼び出されるタイマースレッドが停止しているかを表すフラグ(false:動作中 true:停止中)
-    final private int workSuggestTimeFirst = 1 * 25;//初回の作業中止の提案までの時間(秒)
-    final private int workSuggestTime = 3 * 5;//作業中止の提案の周期(秒)
-    final private int workActionTimeFirst = 0;//初回の作業動作までの時間(秒)
-    final private int workActionTime = 10 * 1;//作業中の動作の周期(秒)
-    final private int breakSuggestTimeFirst = 3 * 5;//初回の休憩中止の提案までの時間(秒)
-    final private int breakSuggestTime = 3 * 5;//休憩中止の提案の周期(秒)
+    final private int workSuggestTimeFirst = 60 * 25;//初回の作業中止の提案までの時間(秒)
+    final private int workSuggestTime = 60 * 5;//作業中止の提案の周期(秒)
+    final private int workActionTimeFirst = 10;//初回の作業動作までの時間(秒)
+    final private int workActionTime = 30;//作業中の動作の周期(秒)
+    final private int breakSuggestTimeFirst = 60 * 5;//初回の休憩中止の提案までの時間(秒)
+    final private int breakSuggestTime = 60 * 5;//休憩中止の提案の周期(秒)
     final private int breakActionTimeFirst = 10;//初回の休憩動作までの時間(秒)
-    final private int breakActionTime = 10 * 1;//休憩中の動作の周期(秒)
+    final private int breakActionTime = 30;//休憩中の動作の周期(秒)
     private boolean phaseFrag;//現在のフェイズを表すフラグ(false:break true:work)
     private boolean alertFrag;//終了予定時刻の通知が済んだかを示すフラグ(false:未　true:済)
     private int alertTimer;//終了予定時刻までの時間をカウントダウンするタイマー
@@ -218,7 +218,6 @@ public class SessionActivity extends Activity implements VoiceUIListenerImpl.Sce
         alertTimer = 3600 * sessionLength;//単位を時間から秒に変換
         Log.v(TAG, "Session AlertTime:" + alertTimer);
         alertFrag = (alertTimer == 0);//アラートまでの時間が未定義等により0秒になった時は、すでにアラート済みということにしてタイマーを止める
-        alertTimer = 36;
 
         //セッション開始の発話
         if(alertFrag) {//時間が設定されていないときは発話内容に時間の含まれていないトピックを呼び出す
@@ -339,7 +338,18 @@ public class SessionActivity extends Activity implements VoiceUIListenerImpl.Sce
                 break;
             case VoiceUIListenerImpl.RESOLVE_VARIABLE:
             case VoiceUIListenerImpl.ACTION_START:
+                accostingFrag = true;//発話中であることを表すフラグ
+                accostingName = VoiceUIVariableUtil.getVariableData(variables, ScenarioDefinitions.TAG_ACCOST);//発話中のシナリオの名前
+                Log.v(TAG, "accostingFrag:" + accostingFrag);
+                break;
             case VoiceUIListenerImpl.ACTION_CANCELLED:
+                Log.v(TAG, "Cancelled Scenario is " + VoiceUIVariableUtil.getVariableData(variables, ScenarioDefinitions.TAG_ACCOST));
+                if(Objects.equals(accostingName, VoiceUIVariableUtil.getVariableData(variables, ScenarioDefinitions.TAG_ACCOST))){//発話が棄却された場合は立てたフラグ等を戻す
+                    accostingFrag = false;//発話中であることを表すフラグ
+                    accostingName = "";//発話中のシナリオの名前
+                    Log.v(TAG, "accostingFrag:" + accostingFrag);
+                }
+                //他のシナリオの発話中に発話が棄却された場合は何もしない
                 break;
             case VoiceUIListenerImpl.ACTION_REJECTED:
                 Log.v(TAG, "Rejected Scenario is " + VoiceUIVariableUtil.getVariableData(variables, ScenarioDefinitions.TAG_ACCOST));
@@ -393,7 +403,7 @@ public class SessionActivity extends Activity implements VoiceUIListenerImpl.Sce
             int result;
             Log.v(TAG, "Try To ACTION");
             if (phaseFrag) {//work状態のとき
-                if(phaseTimer>alertTimer*0.8){//もうちょっと終了時間になる時(80％以上)は"work_actions"のt8~t10で発話させる
+                if(alertTimer < (3600 * sessionLength) * 0.2){//もうちょっとで終了時間になる時(残り時間がセッション時間の20％未満の時)は"work_actions"のt8~t10で発話させる
                     int rnd = new Random().nextInt(3) + 8;//"work_actions"のt8~t10のうちランダムの数字を出す
                     result = VoiceUIManagerUtil.startSpeech(mVUIManager, ScenarioDefinitions.ACC_WORK_ACTIONS + ".t" + rnd);//アクションシナリオを起動する
                     if (Objects.equals(result, VoiceUIManager.VOICEUI_ERROR)) {
